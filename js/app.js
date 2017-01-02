15c7b21748f1834cccae489af7fff018181f6ecc
Anupkumar-Gopi <anupgopi.webdev@gmail.com>
2017-01-02 18:55:50 +0530

    Assignment-6.1

---
+++ js/app.js
@@ -0,0 +1,2 @@
+var r = prompt("Enter radius of circle"); // variable to hold radius input from user
+console.info("Area of Circle with radius of",r," is ",Math.PI*(r**2)); // console output of area
